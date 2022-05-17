6defece9ba7f6a50aea6913f8aabe24cbe3b3895
Rishabh Malhotra <rishabh.malhotra@jungleworks.com>
2022-05-17 16:56:25 +0530

    media query on login

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, AfterContentChecked {
 
   public showNav: boolean = true
 
   constructor() { }
-
-  ngOnInit(): void {
+  ngAfterContentChecked(): void {
     let url = location.href.split('/').pop()
     if (!url || url.toLowerCase() == 'signup') this.showNav = false
     else this.showNav = true
+  }
+
+  ngOnInit(): void {
   }
 
 }
